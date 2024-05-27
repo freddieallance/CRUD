f53f053bebe6d54e76df1651d62b483f81835a04
freddieallance <122672447+freddieallance@users.noreply.github.com>
2024-05-27 16:04:15 +0800

    CRUD

---
+++ WpfCrud/UserControls/EmployeePage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using WpfCrud.Models;
+using WpfCrud.Services;
+
+namespace WpfCrud.UserControls
+{
+    public partial class EmployeePage : UserControl
+    {
+        private Employee selectedEmployee = new Employee();
+        private ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+
+        public EmployeePage()
+        {
+            InitializeComponent();
+            GetEmployees();
+        }
+
+        // Gets all the current employees
+        private void GetEmployees()
+        {
+            employees = DbServices.employeesDbService.Select();
+            EmployeesGridView.ItemsSource = employees;
+        }
+
+        // When an employee is clicked on in the grid view, this method is called and assigns the data to both the selectedEmployee object and the UI fields
+        private void EmployeesGridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (EmployeesGridView.SelectedItem is Employee selected)
+            {
+                selectedEmployee = selected;
+                FirstNameTextBox.Text = selectedEmployee.FirstName;
+                LastNameTextBox.Text = selectedEmployee.LastName;
+                TitleTextBox.Text = selectedEmployee.Title;
+                SalaryTextBox.Text = selectedEmployee.Salary.ToString();
+            }
+        }
+
+        // Gets all the current employees
+        private void ReadEmployeesButton_Click(object sender, RoutedEventArgs e)
+        {
+            GetEmployees();
+        }
+
+        // This method updates the selectedEmployee object with user entered data
+        private void UpdateSelectedEmployee()
+        {
+            selectedEmployee.FirstName = FirstNameTextBox.Text;
+            selectedEmployee.LastName = LastNameTextBox.Text;
+            selectedEmployee.Title = TitleTextBox.Text;
+            if (int.TryParse(SalaryTextBox.Text, out int salary))
+            {
+                selectedEmployee.Salary = salary;
+            }
+        }
+
+        // This method attempts to create an employee
+        private void CreateEmployeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateSelectedEmployee();
+
+            if (DbServices.employeesDbService.Insert(selectedEmployee))
+            {
+                MessageBox.Show("Employee Created!");
+            }
+            else
+            {
+                MessageBox.Show("An error occurred while creating your employee.");
+            }
+        }
+
+        // This method attempts to update an employee
+        private void UpdateEmployeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateSelectedEmployee();
+
+            if (DbServices.employeesDbService.Update(selectedEmployee))
+            {
+                MessageBox.Show("Employee Updated!");
+            }
+            else
+            {
+                MessageBox.Show("An error occurred while updating your employee.");
+            }
+        }
+
+        // This method attempts to delete an employee
+        private void DeleteEmployeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateSelectedEmployee();
+
+            if (DbServices.employeesDbService.Delete(selectedEmployee))
+            {
+                MessageBox.Show("Employee Deleted!");
+            }
+            else
+            {
+                MessageBox.Show("An error occurred while deleting your employee.");
+            }
+        }
+    }
+}
---
+++ WpfCrud/UserControls/ProductPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using WpfCrud.Models;
+using WpfCrud.Services;
+
+namespace WpfCrud.UserControls
+{
+    public partial class ProductPage : UserControl
+    {
+        //selectedProduct is the object referenced for all CRUD operations except for Read
+        private Product selectedProduct = new Product();
+        //Im using an ObservableCollection because it inherits InotifyPropertyChanged
+        private ObservableCollection<Product> products = new ObservableCollection<Product>();
+
+        public ProductPage()
+        {
+            InitializeComponent();
+            //Reads on initialization
+            GetProducts();
+        }
+
+        private void GetProducts() //retrieves all the Products
+        {
+            products = DbServices.productsDbService.Select();
+            ProductsGridView.ItemsSource = products;
+        }
+
+        //This handler is fired when a user clicks on a row on the DataGrid and makes that product the current reference
+        private void ProductsGridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ProductsGridView.SelectedItem is Product selected)
+            {
+                selectedProduct = selected;
+                NameTextBox.Text = selectedProduct.Name;
+                PriceTextBox.Text = selectedProduct.Price.ToString();
+                ReleaseDatePicker.SelectedDate = selectedProduct.ReleaseDate;
+            }
+        }
+
+        private void ReadProductsButton_Click(object sender, RoutedEventArgs e)
+        {
+            GetProducts();
+        }
+
+        //pulls the data from the GUI and updates the properties of the product to be referenced in CRUD Operations
+        private void UpdateSelectedProduct()
+        {
+            selectedProduct.Name = NameTextBox.Text;
+            if (double.TryParse(PriceTextBox.Text, out double price))
+            {
+                selectedProduct.Price = price;
+            }
+            selectedProduct.ReleaseDate = ReleaseDatePicker.SelectedDate ?? DateTime.Now;
+        }
+
+        //Attempts to create a new product in MySQL
+        private void CreateProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateSelectedProduct();
+            if (DbServices.productsDbService.Insert(selectedProduct))
+            {
+                MessageBox.Show("Product Created!");
+            }
+            else
+            {
+                MessageBox.Show("An error occurred while creating your Product.");
+            }
+        }
+
+        //Updates a row in MySQL
+        private void UpdateProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateSelectedProduct();
+            if (DbServices.productsDbService.Update(selectedProduct))
+            {
+                MessageBox.Show("Product Updated!");
+            }
+            else
+            {
+                MessageBox.Show("An error occurred while updating your Product.");
+            }
+        }
+
+        //Deletes a Product
+        private void DeleteProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateSelectedProduct();
+
+            if (DbServices.productsDbService.Delete(selectedProduct))
+            {
+                MessageBox.Show("Product Deleted!");
+            }
+            else
+            {
+                MessageBox.Show("An error occurred while deleting your Product.");
+            }
+        }
+    }
+}
